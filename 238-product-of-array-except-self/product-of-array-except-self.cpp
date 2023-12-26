8cf10656458875939923b2ab6e3a4da8ee65ab63
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2023-12-26 13:19:37 +0530

    Time: 73 ms (6.12%) | Memory: 25.5 MB (25.59%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,40 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
         int n = nums.size();
-        vector<int> ans(n);
-        vector<int> left_Product(n);
-        vector<int> right_Product(n);
-        left_Product[0] = 1;
-        for(int i=1; i<n; i++){
-            left_Product[i] = left_Product[i-1] * nums[i-1];
+        vector<int> pre(n + 1, 1);
+        vector<int> suff(n + 1, 1);
+        vector<int>ans(n,0);
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            pre[i] = pre[i - 1] * nums[i];
         }
-        right_Product[n-1] = 1;
-        for(int i=n-2; i>=0; i--){
-            right_Product[i] = right_Product[i+1] * nums[i+1];
+        suff[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 1; i--)
+        {
+            suff[i] = suff[i + 1] * nums[i];
         }
-        for(int i=0; i<n; i++){
-            ans[i] = left_Product[i] * right_Product[i];
+        for(int i=0;i<n;i++){
+            int a=1;
+            int b=1;
+            if(i>=1){
+                a=pre[i-1];
+            }
+            if(i<=n-1){
+                b=suff[i+1];
+            }
+            ans[i]=a*b;
         }
+        for(int i=0;i<n;i++){
+            cout<<pre[i]<<" ";
+        }cout<<endl;  
+         for(int i=n-1;i>=0;i--){
+            cout<<suff[i]<<" ";
+        }
+        cout<<endl;
         return ans;
     }
 };
