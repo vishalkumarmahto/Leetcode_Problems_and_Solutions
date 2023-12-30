51acb6fe04a6ba9d5bfe033b47240b5d33c995c6
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2023-12-30 18:21:05 +0530

    Time: 416 ms (5.02%) | Memory: 102.2 MB (17.39%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    bool ifPossible(vector<int> nums, int k, int mid){
-        long long int cSum = 0;
-        for(int i = 0; i<mid; i++){
-            cSum+=nums[i];
-        }
-        int l = 0;
-        int i = mid;
-
-        int t = (cSum+k)/mid;
-        if(t>=nums[i-1]) return true;
-        while(i<nums.size()){
-            cSum+=nums[i];
-            cSum-=nums[l];
-            l++;
-            i++;
-            int t = (cSum+k)/mid;
-            if(t>=nums[i-1]) return true;
-        }
-        return false;
-
-    }
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int ans = 0;
-        int s = 1;
-        int e = nums.size();
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(ifPossible(nums,k,mid)){
-                ans = mid;
-                s = mid+1;
+        int ans = 1 , i = 0;
+        long windSum = 0;
+        sort(nums.begin(),nums.end());
+        for(int j = 0 ; j < nums.size() ; j++)
+        {   
+            int target = nums[j];
+            windSum += target;
+            long cond = (long)( j - i + 1 ) * target  - windSum;
+            while(cond > k )
+            {
+                windSum -= nums[i];
+                i++;
+                cond = (long)( j - i + 1 ) * target  - windSum;
             }
-            else e = mid-1;
-        }
-
+            ans = max(ans,j-i+1);
+        } 
         return ans;
     }
 };
