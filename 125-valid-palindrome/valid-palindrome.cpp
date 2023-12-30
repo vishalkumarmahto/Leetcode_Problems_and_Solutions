0cd434c40e0cf1c31230668bcc1ea0e4cf9012c5
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2023-12-30 18:18:14 +0530

    Time: 4 ms (79.84%) | Memory: 7.5 MB (82.94%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int n = size(s);
-        if (n == 1)
-            return true;
-        string str = "";
-        for (int i = 0; i < n; i++)
-            if (isalnum(s[i]))
-                str += s[i];
-        transform(str.begin(), str.end(), str.begin(), ::toupper);
-
-        int i = 0, j = size(str) - 1;
-
-        while (i < j) {
-            if (str[i] != str[j])
-                return false;
-
-            i++, j--;
-        }
-        return true;
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
