df13785a725d85d5bc015545984d33ad459371d3
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2023-12-30 18:16:34 +0530

    Time: 14 ms (6.72%) | Memory: 8.1 MB (11.19%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = size(s);
+        if (n == 1)
+            return true;
+        string str = "";
+        for (int i = 0; i < n; i++)
+            if (isalnum(s[i]))
+                str += s[i];
+        transform(str.begin(), str.end(), str.begin(), ::toupper);
+
+        int i = 0, j = size(str) - 1;
+
+        while (i < j) {
+            if (str[i] != str[j])
+                return false;
+
+            i++, j--;
+        }
+        return true;
+    }
+};
