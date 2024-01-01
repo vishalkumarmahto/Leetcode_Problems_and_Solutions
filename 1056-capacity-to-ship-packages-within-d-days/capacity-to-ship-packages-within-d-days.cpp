7864d1d40f1283580a6d02688fbfd17be299bc27
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-01-01 14:16:19 +0530

    Time: 129 ms (5.17%) | Memory: 31.6 MB (14.48%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+int findDays(vector<int> &weights, int cap) {
+    int days = 1; //First day.
+    int load = 0;
+    int n = weights.size(); //size of array.
+    for (int i = 0; i < n; i++) {
+        if (load + weights[i] > cap) {
+            days += 1; //move to next day
+            load = weights[i]; //load the weight.
+        }
+        else {
+            //load the weight on the same day.
+            load += weights[i];
+        }
+    }
+    return days;
+}
+    int shipWithinDays(vector<int>& weights, int days) {
+    int low = *max_element(weights.begin(), weights.end());
+    int high = accumulate(weights.begin(), weights.end(), 0);
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int numberOfDays = findDays(weights, mid);
+        if (numberOfDays <= days) {
+            //eliminate right half
+            high = mid - 1;
+        }
+        else {
+            //eliminate left half
+            low = mid + 1;
+        }
+    }
+    return low;
+    }
+};
