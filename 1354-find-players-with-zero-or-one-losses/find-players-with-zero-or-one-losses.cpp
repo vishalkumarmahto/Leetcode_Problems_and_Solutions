bf9ffbc4d33ef6c6e6c95b6e9adf60f99dbb827e
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-01-15 14:46:04 +0530

    Time: 406 ms (95.08%) | Memory: 164.9 MB (71.44%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    map<int,int>mp1;
-    map<int,int>mp2;
+   
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-            for(int i=0;i<matches.size();i++)
-        {
-            mp1[matches[i][0]]++;
-            mp2[matches[i][1]]++;
-        }
-        vector<vector<int>>ans;
-        vector<int>temp1;
-        for(auto it: mp1)
-        {
-            if(mp2.find(it.first)==mp2.end()) 
-            {
-                temp1.push_back(it.first);
+         unordered_map<int,int> lost_map;
+         for(int i=0;i<matches.size();i++){
+             int lose = matches[i][1];
+             lost_map[lose]++;
+         }
+         vector<int> notLost;
+         vector<int> onceLost;
+         for(int i=0;i<matches.size();i++){
+            int winner = matches[i][0];
+            int looser = matches[i][1];
+            if(lost_map.find(winner)==lost_map.end()){
+                notLost.push_back(winner);
+                lost_map[winner]=2;
             }
-//if find function finds the value then it return iterator refering to end of map so if it = returns mp2.end() then our value doesn't exist in mp2 and we add it in list 1(temp1).
-        }
-        ans.push_back(temp1);
-        vector<int>temp2;
-        for(auto it: mp2)
-        {
-// players with only single loss
-            if(it.second==1)
-            {
-                temp2.push_back(it.first);
-            }
-        }
-        ans.push_back(temp2);
-        return ans;
+            if(lost_map[looser]==1)
+             onceLost.push_back(looser);
+         }
+         sort(notLost.begin(),notLost.end());
+         sort(onceLost.begin(), onceLost.end());
+         return {notLost,onceLost};
+
     } 
     
 };
