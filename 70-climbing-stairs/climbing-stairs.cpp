bf191522ff0ab13ad4edf521f6e29777104756f0
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-01-18 12:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (71.72%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp0=1,dp1=1,dp=1;
+        for(int i=2;i<=n;i++){
+            dp = dp0+dp1;
+            dp0=dp1;
+            dp1=dp;
+        }
+        return dp;
+    }
+};
