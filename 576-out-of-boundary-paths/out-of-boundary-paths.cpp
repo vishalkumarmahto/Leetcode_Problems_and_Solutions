b2d0b6649c05b3a7d7975c66e969888b477af153
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-01-26 15:02:11 +0530

    Time: 10 ms (42.77%) | Memory: 10.5 MB (23.29%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findPaths(int m, int n, int N, int x, int y) {
-          const int M = 1000000000 + 7;
+        const int M = 1000000000 + 7;
         vector<vector<int>> dp(m, vector<int>(n, 0));
         dp[x][y] = 1;
         int count = 0;
@@ -11,19 +11,26 @@ public:
 
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (i == m - 1) count = (count + dp[i][j]) % M;
-                    if (j == n - 1) count = (count + dp[i][j]) % M;
-                    if (i == 0) count = (count + dp[i][j]) % M;
-                    if (j == 0) count = (count + dp[i][j]) % M;
-                    temp[i][j] = (
-                        ((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M +
-                        ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M
-                    ) % M;
+                    if (i == m - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (j == n - 1)
+                        count = (count + dp[i][j]) % M;
+                    if (i == 0)
+                        count = (count + dp[i][j]) % M;
+                    if (j == 0)
+                        count = (count + dp[i][j]) % M;
+                    temp[i][j] = (((i > 0 ? dp[i - 1][j] : 0) +
+                                   (i < m - 1 ? dp[i + 1][j] : 0)) %
+                                      M +
+                                  ((j > 0 ? dp[i][j - 1] : 0) +
+                                   (j < n - 1 ? dp[i][j + 1] : 0)) %
+                                      M) %
+                                 M;
                 }
             }
             dp = temp;
         }
 
-        return count; 
+        return count;
     }
 };
