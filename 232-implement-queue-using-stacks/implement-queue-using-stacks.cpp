92bd140ea6d2bc99066c668d05abc3bbbda24c74
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-01-29 10:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.21%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,35 +1,52 @@
 class MyQueue {
-    private:
-    stack<int>s1,s2;
 public:
+stack<int> input;
+stack<int> output;
     MyQueue() {
         
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s1.push(x);
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
+        input.push(x);
     }
     
     int pop() {
-        int temp = s1.top();
-        s1.pop();
-        return temp;
+        if(!output.empty()){
+            int ele = output.top();
+            output.pop();
+            return ele;
+        }
+        else{
+            while(!input.empty()){
+                int ele = input.top();
+                input.pop();
+                output.push(ele);
+            }
+            int ele = output.top();
+            output.pop();
+            return ele;
+        }
     }
     
     int peek() {
-        return s1.top();
+        if(!output.empty()){
+            return output.top();
+        }
+        else{
+            while(!input.empty()){
+                int ele = input.top();
+                input.pop();
+                output.push(ele);
+            }
+            return output.top();
+        }
+        
     }
     
     bool empty() {
-        return s1.empty();
+        if(input.empty() and output.empty()) return true;
+        else return false;
+        
     }
 };
 
