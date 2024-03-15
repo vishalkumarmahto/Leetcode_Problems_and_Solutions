5cb60becd3754ebeeab00122f341e51d958db151
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-03-15 10:34:33 +0530

    Time: 20 ms (30.01%) | Memory: 27.2 MB (43.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,40 +1,22 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> productExceptSelf(vector<int> &nums)
-    {
+    vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pre(n + 1, 1);
-        vector<int> suff(n + 1, 1);
-        vector<int>ans(n,0);
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++)
-        {
-            pre[i] = pre[i - 1] * nums[i];
+        vector<int> output;
+        if (n < 1)
+            return output;
+
+        int product = 1;
+        for (int i = 0; i < n; i++) {
+            product *= nums[i];
+            output.push_back(product);
         }
-        suff[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 1; i--)
-        {
-            suff[i] = suff[i + 1] * nums[i];
+        product = 1;
+        for (int i = n - 1; i > 0; i--) {
+            output[i] = output[i - 1] * product;
+            product *= nums[i];
         }
-        for(int i=0;i<n;i++){
-            int a=1;
-            int b=1;
-            if(i>=1){
-                a=pre[i-1];
-            }
-            if(i<=n-1){
-                b=suff[i+1];
-            }
-            ans[i]=a*b;
-        }
-        for(int i=0;i<n;i++){
-            cout<<pre[i]<<" ";
-        }cout<<endl;  
-         for(int i=n-1;i>=0;i--){
-            cout<<suff[i]<<" ";
-        }
-        cout<<endl;
-        return ans;
+        output[0] = product;
+        return output;
     }
 };
