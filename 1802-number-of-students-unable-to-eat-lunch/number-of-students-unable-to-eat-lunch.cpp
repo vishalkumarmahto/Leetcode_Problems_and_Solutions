b8fc51cfd4467432cbb5b45a0b8f38ebb0ded739
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-04-08 08:50:44 +0530

    Time: 4 ms (31.92%) | Memory: 10.7 MB (79.46%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        int n = students.size();
+        int arr[2] = {0};
+        for (int& stud : students) {
+            arr[stud]++;
+        }
+        for (int i = 0; i < n; i++) {
+            int sand = sandwiches[i];
+            if (arr[sand] == 0) {
+                return n - i;
+            }
+            arr[sand]--;
+        }
+        return 0;
+    }
+};
