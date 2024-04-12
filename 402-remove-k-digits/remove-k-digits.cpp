f0f24283c6f521d20f2d847418f31ab0f7c44a17
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-04-12 12:01:13 +0530

    Time: 4 ms (97.63%) | Memory: 9.6 MB (96.24%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        // Stack to store the digits
-        stack<char> numStack;
-
-        // Iterating through each digit in the number
-        for (char digit : num) {
-            // While there are remaining removals (k) and the stack is not empty
-            // and the current digit is smaller than the top of the stack
-            while (k > 0 && !numStack.empty() && digit < numStack.top()) {
-                // Remove digits from the stack
-                numStack.pop();
+        int n = num.length();
+        string ans = "";
+        for (int i = 0; i < n; i++) {
+            while (ans.length() > 0 && ans.back() > num[i] && k > 0) {
+                ans.pop_back();
                 k--;
             }
-            // Push the current digit to the stack
-            numStack.push(digit);
-        }
-
-        // After iterating through the number, if there are still remaining
-        // removals, remove digits from the stack
-        while (k > 0 && !numStack.empty()) {
-            numStack.pop();
-            k--;
-        }
-
-        // Construct the result string by popping digits from the stack
-        string temp = "";
-        while (!numStack.empty()) {
-            temp.push_back(numStack.top());
-            numStack.pop();
-        }
-        // Reverse the result string to get the correct order
-        reverse(temp.begin(), temp.end());
-
-        // Remove leading zeros and construct the final result
-        int m = temp.size();
-        string result = "";
-        int foundNonZero = 0;
-        for (int i = 0; i < m; i++) {
-            if (temp[i] == '0' && foundNonZero == 0) {
-                continue;
-            } else {
-                result.push_back(temp[i]);
-                foundNonZero = 1;
+            if (ans.length() > 0 || num[i] != '0') {
+                ans.push_back(num[i]);
             }
         }
-        // If the result is empty, return "0"
-        if (result.size() == 0)
-            result.push_back('0');
-        return result;
+        while (ans.length() > 0 && k > 0) {
+            ans.pop_back();
+            k--;
+        }
+        if (ans == "") {
+            return "0";
+        }
+
+        return ans;
     }
 };
