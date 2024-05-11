07025e5e278764cf58b956c9ecb2581f5469dd5a
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-05-11 20:59:44 +0530

    Time: 3 ms (95.13%) | Memory: 12.9 MB (6.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,7 +4,9 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> prev(m,0);
         for (int i = 0; i < n; i++) {
+            vector<int> cur(m,0);       
             for (int j = 0; j < m; j++) {
                 if (i == 0 && j == 0)
                     dp[i][j] = grid[i][j];
