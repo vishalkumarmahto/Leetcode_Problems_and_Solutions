dae91bf61b50a7856861060c82dc41ac79ea4784
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-06-11 12:20:19 +0530

    Time: 3 ms (53.89%) | Memory: 10.8 MB (6.31%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < arr2.size(); i++) {
+            mp[arr2[i]] = i;
+        }
         for (int& num : arr1) {
-            mp[num]++;
-        }
-        int i = 0;
-        for (int& num : arr2) {
-            while (mp[num]-- > 0) {
-                arr1[i++] = num;
+            if (!mp.count(num)) {
+                mp[num] = 1e9;
             }
         }
-        for (auto& it : mp) {
-            int freq = it.second;
-            while (freq > 0) {
-                arr1[i++] = it.first;
-                freq--;
+        auto lambda = [&](int num1, int num2) {
+            if (mp[num1] == mp[num2]) {
+                return num1 < num2;
             }
-        }
+            return mp[num1] < mp[num2];
+        };
+        sort(arr1.begin(), arr1.end(), lambda);
         return arr1;
     }
 };
