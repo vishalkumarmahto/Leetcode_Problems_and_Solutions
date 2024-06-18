14e6ddec3c41e6f7e6930e514dc5917a4a075f35
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-06-18 17:55:09 +0530

    Time: 58 ms (42.01%) | Memory: 41.1 MB (57.35%) - LeetSync

---
+++ 853-most-profit-assigning-work/most-profit-assigning-work.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
+                            vector<int>& worker) {
+        int n = difficulty.size();
+        int m = worker.size();
+
+        priority_queue<pair<int, int>> pq;
+        for (int i = 0; i < n; i++) {
+            int diff = difficulty[i];
+            int prof = profit[i];
+            pq.push({prof, diff});
+        }
+        sort(worker.begin(), worker.end(), greater<int>());
+
+        int i = 0;
+        int totalProfit = 0;
+        while (i < m && !pq.empty()) {
+            if (pq.top().second > worker[i]) {
+                pq.pop();
+            } else {
+                totalProfit += pq.top().first;
+                i++;
+            }
+        }
+        return totalProfit;
+    }
+};
