fc82f3a13f68afcc17f719656d5f0e70f092ae1c
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-07-06 21:11:34 +0530

    Time: 3 ms (16.54%) | Memory: 7 MB (96.49%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int idx = 1;
-        int dir = 1;
-        while(time>0){
-            if(idx + dir >=1 && idx + dir<=n){
-                idx+=dir;
-                time--;
-            }
-            else{
-                dir*=-1;
-            }
+        int fullRounds = time/(n-1);
+
+        int timeLeft = time % (n-1);
+        if(fullRounds % 2==0){
+            // you must be standing at first position
+            return timeLeft + 1;
         }
-        return idx;
+        else{
+           return n - timeLeft;
+        }
+        return -1;
     }
 };
