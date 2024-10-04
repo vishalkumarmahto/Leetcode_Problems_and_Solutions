e887944d3335c34b3c8feb862e3a86f6eacea284
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-10-04 09:17:01 +0530

    Time: 48 ms (98.20%) | Memory: 56.1 MB (97.00%) - LeetSync

---
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int>& skill) {
+        int n=skill.size(), n_2=n/2;
+        long long sum=0;
+        int freq[1001]={0}, xMax=1, xMin=1000;
+        for(int x: skill){
+            sum+=x;
+            freq[x]++;
+            xMax=max(x, xMax);
+            xMin=min(x, xMin);
+        }
+        if (sum%n_2!=0) return -1;
+        int team_skill=sum/n_2;
+        
+        long long chemi=0;
+        int l, r;
+        for(l=xMin, r=xMax; l<r; l++, r--){
+            long long fL=freq[l], fR=freq[r];
+            if (l+r!=team_skill || fL!=fR) return -1;
+            chemi+=fL*l*r;
+        }
+        chemi+=(l==r && l*2==team_skill)?freq[l]/2LL*l*l:0LL;
+
+        return chemi;
+    }
+};
