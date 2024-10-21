1a4f38f69eb3b025d5ec935c41dab5dec20a124a
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-10-21 13:28:34 +0530

    Time: 13 ms (98.30%) | Memory: 21.6 MB (91.89%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    long long isValid(int mid, vector<int>& piles, int h) {
-        long long curr = 0;
-        for (auto &p:piles) {
-            if (p <= mid) curr += 1;
-            else {
-                curr += p / mid;
-                if (p % mid != 0) curr += 1;
-            }
+    long long isValid(vector<int>& piles, int& hourly) {
+        long long totalHours = 0;
+        for (int i = 0; i < piles.size(); i++) {
+             totalHours += (piles[i] + hourly - 1) / hourly;
         }
-        return curr;
+        return totalHours;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1, high = 10000000000;
-        int mid;
-        int k;
+        int maxEl = *max_element(piles.begin(), piles.end());
+        int low = 1, high = maxEl;
         while (low <= high) {
-            mid = (low + high) / 2;
-            k = isValid(mid, piles, h);
-            if (k <= h) {
+            int mid = low + (high - low) / 2;
+            if (isValid(piles, mid) <= h) {
                 high = mid - 1;
-            }
-            if (k > h) low = mid + 1;
+            } else
+                low = mid + 1;
         }
         return low;
     }
