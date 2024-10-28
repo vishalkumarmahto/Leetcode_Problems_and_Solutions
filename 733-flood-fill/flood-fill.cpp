71596f1d3e213bc10d384b8cee5cd3ff5fc5ea9a
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-10-28 13:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (94.60%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,13 +1,15 @@
 class Solution {
+
 private:
     void dfs(int row, int col, vector<vector<int>>& ans,
              vector<vector<int>>& image, int newColor, int delRow[],
              int delCol[], int iniColor) {
+
         // color with new color
         ans[row][col] = newColor;
         int n = image.size();
         int m = image[0].size();
-        // there are exactly 4 neighbours
+        // there are exctly 4 neighbours
         for (int i = 0; i < 4; i++) {
             int nrow = row + delRow[i];
             int ncol = col + delCol[i];
@@ -22,14 +24,14 @@ private:
 
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int newColor) {
+                                  int color) {
         // get initial color
         int iniColor = image[sr][sc];
         vector<vector<int>> ans = image;
         // delta row and delta column for neighbours
         int delRow[] = {-1, 0, +1, 0};
         int delCol[] = {0, +1, 0, -1};
-        dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
+        dfs(sr, sc, ans, image, color, delRow, delCol, iniColor);
         return ans;
     }
 };
