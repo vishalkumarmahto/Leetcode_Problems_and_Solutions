17276c25df13da3c274a956e5cb23bc0fc4643a8
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-10-30 10:44:34 +0530

    Time: 7 ms (95.74%) | Memory: 38.3 MB (33.41%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-
-    void bfs(queue<vector<int>> q, vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
-
-        while (q.size() != 0) {
-            vector<int> a = q.front();
-            q.pop();
-            int r = a[0];
-            int c = a[1];
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dir[k][0];
-                int nc = c + dir[k][1];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
-                    arr[nr][nc] == -1) {
-                    arr[nr][nc] = arr[r][c] + 1;
-                    q.push({nr, nc});
-                }
-            }
-        }
-    }
-    vector<vector<int>> updateMatrix(vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
-
-        queue<vector<int>> q;
-
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        // visited the distance matrix
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, 0));
+        // <coordinates, steps>
+        queue<pair<pair<int, int>, int>> q;
+        // traverse the matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (arr[i][j] == 0) {
-                    q.push({i, j});
+                // start BFS if cell contains 0
+                if (mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
                 } else {
-                    arr[i][j] = -1;
+                    // mark unvisited
+                    vis[i][j] = 0;
                 }
             }
         }
-
-        bfs(q, arr);
-        return arr;
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, 0, -1};
+        // traverse till queue becomes empty
+        while (!q.empty()) {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+            dist[row][col] = steps;
+            // for all 4 neighbours
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                // check for valid unvisited cell
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    vis[nrow][ncol] == 0) {
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow, ncol}, steps + 1});
+                }
+            }
+        }
+        return dist;
     }
 };
