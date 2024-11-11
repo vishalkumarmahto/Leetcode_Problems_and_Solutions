1207fb8c8a882429b64dab4d8547190b855f9de6
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-11 14:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (20.46%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int countWays(int m, int n, vector<vector<int>>& dp) {
-        if (m == 0 && n == 0)
-            return 1;
-        if (m < 0 || n < 0)
-            return 0;
-        if (dp[m][n] != -1)
-            return dp[m][n];
-        int up = countWays(m - 1, n, dp);
-        int left = countWays(m, n - 1, dp);
-        return dp[m][n] = up + left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countWays(m - 1, n - 1, dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int up=0,left=0;
+                if(i==0&&j==0) dp[0][0] = 1;
+                
+                else{
+                    if(i>0)  up = dp[i-1][j];
+                   if(j>0)   left = dp[i][j-1];
+                      dp[i][j] = left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
