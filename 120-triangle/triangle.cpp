4a75e3a94fcf7d315b93ad13d620fa9584498bcc
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-12 14:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (8.85%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,22 +1,18 @@
 class Solution {
-private:
-    int f(int i, int j, vector<vector<int>>& triangle, int n,
-          vector<vector<int>>& dp) {
-        if (i == n - 1)
-            return triangle[i][j];
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int down = triangle[i][j] + f(i + 1, j, triangle, n, dp);
-        int dg = triangle[i][j] + f(i + 1, j + 1, triangle, n, dp);
-        return dp[i][j] = min(down, dg);
-    }
-
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        return f(0, 0, triangle, n, dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down,dg);
+            }
+        }
+        return dp[0][0];
     }
 };
