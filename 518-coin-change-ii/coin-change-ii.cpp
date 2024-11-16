d96725153a86822c8f1941a01720604574532649
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-16 18:42:46 +0530

    Time: 21 ms (52.73%) | Memory: 8.9 MB (95.68%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,31 +1,28 @@
+
 class Solution {
 public:
-  
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long long>> dp(n, vector<long long>(amount + 1, 0)); // Use long long for safety
-
-        // Base case: If the target is 0, there is exactly 1 way (use no coins)
+       vector<int>prev(amount+1,0),curi(amount+1,0);
         for (int tar = 0; tar <= amount; tar++) {
             if (tar % coins[0] == 0)
-                dp[0][tar] = 1; // Use one type of coin repeatedly
+                prev[tar] = 1;
+            else
+                prev[tar] = 0;
         }
-
-        // Fill the DP table
         for (int ind = 1; ind < n; ind++) {
             for (int tar = 0; tar <= amount; tar++) {
-                long long notTake = dp[ind - 1][tar]; // Do not include the current coin
+                long long notTake = prev[tar];
                 long long take = 0;
-                if (coins[ind] <= tar) // Include the current coin
-                    take = dp[ind][tar - coins[ind]];
-                dp[ind][tar] = take + notTake;
-
-                // Avoid potential overflow issues
-                if (dp[ind][tar] > INT_MAX)
-                    dp[ind][tar] = INT_MAX;
+                if (coins[ind] <= tar)
+                    take = curi[tar - coins[ind]];
+                curi[tar] = take + notTake;
+                if (curi[tar] > INT_MAX)
+                    curi[tar] = INT_MAX;
             }
+            prev = curi;
         }
 
-        return dp[n - 1][amount] > INT_MAX ? -1 : dp[n - 1][amount];
+        return prev[amount];
     }
 };
