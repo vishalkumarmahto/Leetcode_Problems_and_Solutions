d0bd5b2a4f187dc4989e1f9bfdb4657353b04701
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-18 14:17:00 +0530

    Time: 59 ms (69.68%) | Memory: 8.3 MB (93.45%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int lcs(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 0;
+        vector<int>prev(m+1,0),curi(m+1,0);
+     
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s1[i - 1] == s2[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                   curi[j] = 1 +prev[j - 1];
                 else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    curi[j] = max(prev[j], curi[j - 1]);
             }
+            prev = curi;
         }
-        return dp[n][m];
+        return prev[m];
     }
     int longestPalindromeSubseq(string s) {
         string t = s;
