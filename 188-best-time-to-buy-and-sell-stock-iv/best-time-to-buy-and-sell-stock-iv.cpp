03e5659e505228d1118eb2e3c06d099b91ee6b83
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-23 23:18:48 +0530

    Time: 3 ms (83.92%) | Memory: 13.2 MB (97.45%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,37 +1,22 @@
 class Solution {
-private:
-    int f(int ind, int trans, int k, vector<int>& prices,
-          vector<vector<int>>& dp) {
-        if (ind == prices.size() || trans == 2 * k)
-            return 0;
-        if (dp[ind][trans] != -1)
-            return dp[ind][trans];
-        if (trans % 2 == 0) {
-            return dp[ind][trans] =
-                       max(-prices[ind] + f(ind + 1, trans + 1, k, prices, dp),
-                           0 + f(ind + 1, trans, k, prices, dp));
-        } else {
-            return dp[ind][trans] =
-                       max(prices[ind] + f(ind + 1, trans + 1, k, prices, dp),
-                           0 + f(ind + 1, trans, k, prices, dp));
-        }
-    }
+
 
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int>(2 * k + 1, 0));
+        vector<int>after(2*k+1,0),curi(2*k+1,0);
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int trans = 2 * k - 1; trans >= 0; trans--) {
                 if (trans % 2 == 0) {
-                    dp[ind][trans] = max(-prices[ind] + dp[ind + 1][trans + 1],
-                                         0 + dp[ind + 1][trans]);
+                    curi[trans] = max(-prices[ind] + after[trans + 1],
+                                         0 + after[trans]);
                 } else {
-                    dp[ind][trans] = max(prices[ind] + dp[ind + 1][trans + 1],
-                                         0 + dp[ind + 1][trans]);
+                    curi[trans] = max(prices[ind] + after[trans + 1],
+                                         0 + after[trans]);
                 }
             }
+            after = curi;
         }
-        return dp[0][0];
+        return curi[0];
     }
 };
