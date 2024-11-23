5c3152bd8470667a0689a37bc92e4f38b035a21c
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-23 20:48:55 +0530

    Time: 54 ms (64.49%) | Memory: 78.2 MB (82.20%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,45 +1,27 @@
 class Solution {
-private:
-    int f(int ind, int buy, int cap, vector<int>& prices,
-          vector<vector<vector<int>>>& dp) {
-        if (ind == prices.size())
-            return 0;
-        if (cap == 0)
-            return 0;
-        int profit = 0;
-        if (dp[ind][buy][cap] != -1)
-            return dp[ind][buy][cap];
-        if (buy) {
-            profit = max(-prices[ind] + f(ind + 1, 0, cap, prices, dp),
-                         0 + f(ind + 1, 1, cap, prices, dp));
-        } else {
-            profit = max(prices[ind] + f(ind + 1, 1, cap - 1, prices, dp),
-                         0 + f(ind + 1, 0, cap, prices, dp));
-        }
-        return dp[ind][buy][cap] = profit;
-    }
-
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(
-            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+      vector<vector<int>>after(2, vector<int>(3, 0));
+      vector<vector<int>>curi(2, vector<int>(3, 0));
+
         for (int ind = n - 1; ind >= 0; ind--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = 1; cap <= 2; cap++) {
                     long profit = 0;
 
                     if (buy) {
-                        profit = max(-prices[ind] + dp[ind + 1][0][cap],
-                                     0 + dp[ind + 1][1][cap]);
+                        profit = max(-prices[ind] + after[0][cap],
+                                     0 + after[1][cap]);
                     } else {
-                        profit = max(prices[ind] + dp[ind + 1][1][cap - 1],
-                                     0 + dp[ind + 1][0][cap]);
+                        profit = max(prices[ind] + after[1][cap - 1],
+                                     0 + after[0][cap]);
                     }
-                    dp[ind][buy][cap] = profit;
+                    curi[buy][cap] = profit;
                 }
             }
+            after = curi;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
