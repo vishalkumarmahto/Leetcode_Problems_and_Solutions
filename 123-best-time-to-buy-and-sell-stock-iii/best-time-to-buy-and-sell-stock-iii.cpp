5dd9adfd7d367dcf12cba0c2af5f7cc99929b4be
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-23 22:31:53 +0530

    Time: 131 ms (57.42%) | Memory: 126.2 MB (58.47%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,41 @@
 class Solution {
-    private:
-    int f(int ind, int trans, vector<int>&prices,vector<vector<int>>&dp){
-        if(ind==prices.size()||trans==4) return 0;
+private:
+    int f(int ind, int trans, vector<int>& prices, vector<vector<int>>& dp) {
+        if (ind == prices.size() || trans == 4)
+            return 0;
         int profit = 0;
-        if(dp[ind][trans]!=-1) return dp[ind][trans];
-        if(trans%2==0) // buy
+        if (dp[ind][trans] != -1)
+            return dp[ind][trans];
+        if (trans % 2 == 0) // buy
         {
-            profit = max(-prices[ind]+ f(ind+1,trans+1,prices,dp),0+f(ind+1,trans,prices,dp));
+            profit = max(-prices[ind] + f(ind + 1, trans + 1, prices, dp),
+                         0 + f(ind + 1, trans, prices, dp));
 
-        }
-        else{
-            profit = max(prices[ind]+f(ind+1,trans+1,prices,dp), 0 + f(ind+1,trans,prices,dp));
+        } else {
+            profit = max(prices[ind] + f(ind + 1, trans + 1, prices, dp),
+                         0 + f(ind + 1, trans, prices, dp));
         }
         return dp[ind][trans] = profit;
     }
+
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>>dp(n,vector<int>(4,-1));
-        return f(0,0,prices,dp);
-
-
+        vector<vector<int>> dp(n + 1, vector<int>(4, 0));
+       
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int trans = 3; trans >= 0; trans--) {
+                int profit = 0;
+               if (trans % 2 == 0) { // Buy
+                    profit = max(-prices[ind] + ((trans + 1 <= 3) ? dp[ind + 1][trans + 1] : 0), 
+                                 0 + dp[ind + 1][trans]); // Skip
+                } else { // Sell
+                    profit = max(prices[ind] + ((trans + 1 <= 3) ? dp[ind + 1][trans + 1] : 0), 
+                                 0 + dp[ind + 1][trans]); // Skip
+                }
+                dp[ind][trans] = profit;
+            }
+        }
+        return dp[0][0];
     }
 };
