0fe27bd6b5a9822079f43d64d8051e76e4042214
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-24 11:34:55 +0530

    Time: 150 ms (34.23%) | Memory: 13.4 MB (39.69%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1e5);
-        for (int i = 0; i < n; i++) {
-            int p = lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
-            if (p < n)
-                dp[p] = min(dp[p], nums[i]);
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> next(n + 1, 0), curi(n + 1, 0);
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev_ind = ind - 1; prev_ind >= -1; prev_ind--) {
+                int len = 0 + next[prev_ind + 1];
+                if (prev_ind == -1 || arr[ind] > arr[prev_ind])
+                    len = max(len, 1 + next[ind + 1]);
+                curi[prev_ind + 1] = len;
+            }
+            next = curi;
         }
-        for (int i = n - 1; i > -1; i--) {
-            if (dp[i] < 1e5)
-                return i + 1;
-        }
-        return 0;
+        return next[-1 + 1];
     }
 };
