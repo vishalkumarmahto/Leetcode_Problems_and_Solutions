d0f91bb8449f402ebaa56ce28128f9e0af88136f
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-11-28 11:21:36 +0530

    Time: 1529 ms (35.46%) | Memory: 9.1 MB (55.18%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -9,26 +9,23 @@ private:
         }
         return true;
     }
-    int f(int i, int n, string& s,vector<int>&dp) {
-        if (i == n)
-            return 0;
-        string temp = "";
-        if(dp[i]!=-1) return dp[i];
-        int mini = INT_MAX;
-        for (int j = i; j < n; j++) {
-            temp += s[j];
-            if (isPalindrome(i, j, s)) {
-                int cost = 1 + f(j + 1, n, s,dp);
-                mini = min(mini, cost);
-            }
-        }
-        return dp[i]= mini;
-    }
 
 public:
     int minCut(string s) {
         int n = s.size();
-        vector<int>dp(n,-1);
-        return f(0, n, s,dp)-1;
+        vector<int> dp(n + 1, 0);
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int mini = INT_MAX;
+            for (int j = i; j < n; j++) {
+
+                if (isPalindrome(i, j, s)) {
+                    int cost = 1 + dp[j + 1];
+                    mini = min(mini, cost);
+                }
+            }
+            dp[i] = mini;
+        }
+        return dp[0] - 1;
     }
 };
