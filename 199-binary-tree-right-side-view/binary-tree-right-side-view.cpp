f41918788e5a9ff1008f86b08ad793ac0db95a8d
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-12-01 16:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (11.44%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,22 +11,22 @@
  * };
  */
 class Solution {
-public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        dfs(root, 0, ans);
-        return ans;
-    }
-
-    void dfs(TreeNode* root, int level, vector<int>& ans) {
-        if (root == NULL)
+private:
+    void recursion(TreeNode* root, int level, vector<int>& ans) {
+        if (root == NULL) {
             return;
-
+        }
         if (level == ans.size()) {
             ans.push_back(root->val);
         }
+        recursion(root->right, level + 1, ans);
+        recursion(root->left, level + 1, ans);
+    }
 
-        dfs(root->right, level + 1, ans);
-        dfs(root->left, level + 1, ans);
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        recursion(root, 0, ans);
+        return ans;
     }
 };
