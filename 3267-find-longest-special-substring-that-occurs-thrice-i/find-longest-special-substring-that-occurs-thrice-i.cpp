aae95ebb85f2304c55aadc1885b87d9b9aa15c3b
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-12-10 19:28:05 +0530

    Time: 2 ms (97.47%) | Memory: 9.8 MB (95.32%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    bool helper(const string& s, int n, int x) {
+        vector<int> cnt(26, 0);
+        int p = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (s[p] != s[i])
+                p++;
+            if (i - p + 1 >= x)
+                cnt[s[i] - 'a']++;
+            if (cnt[s[i] - 'a'] > 2)
+                return true;
+        }
+
+        return false;
+    }
+
+public:
+    int maximumLength(string s) {
+        int n = s.size();
+        int l = 1, r = n;
+
+        if (!helper(s, n, l))
+            return -1;
+
+        while (l + 1 < r) {
+            int mid = (l + r) / 2;
+            if (helper(s, n, mid))
+                l = mid;
+            else
+                r = mid;
+        }
+
+        return l;
+    }
+};
