176a698c9d2ac2679470c11a5367483ebc05ff97
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-12-11 20:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (62.12%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,32 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
+private:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+
 public:
-    void inorder(TreeNode* root, vector<TreeNode*>& nodes) {
-        if (root == nullptr) return;
-        inorder(root->left, nodes);
-        nodes.push_back(root); // Collect node references
-        inorder(root->right, nodes);
+    void inorder(TreeNode* root) {
+        if (root == NULL)
+            return;
+        inorder(root->left);
+        if (prev != NULL && (root->val < prev->val)) {
+            if (first == NULL) {
+                first = prev;
+                middle = root;
+            } else
+                last = root;
+        }
+        prev = root;
+        inorder(root->right);
     }
-
     void recoverTree(TreeNode* root) {
-        vector<TreeNode*> nodes;
-        inorder(root, nodes);
-        
-        TreeNode* first = nullptr;
-        TreeNode* second = nullptr;
-
-        // Find the two swapped nodes
-        for (int i = 0; i < nodes.size() - 1; ++i) {
-            if (nodes[i]->val > nodes[i + 1]->val) {
-                if (first == nullptr) {
-                    first = nodes[i];
-                }
-                second = nodes[i + 1];
-            }
-        }
-
-        // Swap their values
-        if (first && second) {
-            swap(first->val, second->val);
-        }
+        first = middle = last = NULL;
+        prev = new TreeNode(INT_MIN);
+        inorder(root);
+        if (first && last)
+            swap(first->val, last->val);
+        else if (first && middle)
+            swap(first->val, middle->val);
     }
 };
