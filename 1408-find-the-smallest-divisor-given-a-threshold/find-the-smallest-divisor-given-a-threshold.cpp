82d9ecde6d9f8ca32fd421aeda5e57e3111e0436
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2024-12-16 18:35:54 +0530

    Time: 5 ms (85.50%) | Memory: 26.2 MB (20.62%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-
-int sumByD(vector<int> &nums, int div) {
-    int n = nums.size(); //size of array
-    //Find the summation of division values:
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += ceil((double)(nums[i]) / (double)(div));
+    int sumOfDiv(vector<int>& nums, int day, int threshold) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += ceil((double)nums[i] / (double)day);
+        }
+        return sum;
     }
-    return sum;
-}
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
-    if (n > threshold) return -1;
-    int low = 1, high = *max_element(nums.begin(), nums.end());
-
-    //Apply binary search:
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        if (sumByD(nums, mid) <= threshold) {
-            high = mid - 1;
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumOfDiv(nums, mid, threshold) <= threshold) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
-        else {
-            low = mid + 1;
-        }
-    }
-    return low;
-  
+        return low;
     }
 };
