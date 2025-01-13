e1bc6bbe2c550b15fadc5c4c32634aa8596bd502
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-01-14 01:05:23 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (68.71%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    vector<int> getLeftMax(vector<int>& height, int& n) {
-        vector<int> leftMax(n);
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = max(leftMax[i - 1], height[i]);
+    int trap(vector<int>& arr) {
+        int n = arr.size();
+        int leftMax = 0, rightMax = 0, total = 0, left = 0, right = n - 1;
+        while (left <= right) {
+            if (arr[left] <= arr[right]) {
+                if (leftMax > arr[left]) {
+                    total += leftMax - arr[left];
+                } else {
+                    leftMax = arr[left];
+                }
+                left++;
+            } else {
+
+                if (rightMax > arr[right]) {
+                    total += rightMax - arr[right];
+                } else {
+                    rightMax = arr[right];
+                }
+                right--;
+            }
         }
-        return leftMax;
-    }
-    vector<int> getRightMax(vector<int>& height, int& n) {
-        vector<int> rightMax(n);
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-        }
-        return rightMax;
-    }
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> leftMax = getLeftMax(height, n);
-        vector<int> rightMax = getRightMax(height, n);
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int h = min(leftMax[i], rightMax[i]) - height[i];
-            sum += h;
-        }
-        return sum;
+        return total;
     }
 };
