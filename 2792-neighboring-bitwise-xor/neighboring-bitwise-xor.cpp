b65a63feec69c0901fdd7866f3563bc58eab3398
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-01-17 13:11:50 +0530

    Time: 65 ms (5.14%) | Memory: 289.9 MB (5.61%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        vector<int> original = {0};
+        for (int i = 0; i < derived.size(); i++) {
+            original.push_back((derived[i] ^ original[i]));
+        }
+        // Store the validation results in checkForZero and checkForOne
+        // respectively.
+        bool checkForZero = (original[0] == original[original.size() - 1]);
+        original = {1};
+        for (int i = 0; i < derived.size(); i++) {
+            original.push_back((derived[i] ^ original[i]));
+        }
+        bool checkForOne = (original[0] == original[original.size() - 1]);
+
+        return checkForZero | checkForOne;
+    }
+};
