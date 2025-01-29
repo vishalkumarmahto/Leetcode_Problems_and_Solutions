ad80f0322bbf4855d7b0eb1965e0dbaa296af40a
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-01-29 11:11:28 +0530

    Time: 5 ms (21.41%) | Memory: 14.2 MB (20.29%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isConnected(int src, int target, vector<bool>&visited, vector<int>adjList[]){
+        visited[src] = true;
+        if(src==target){
+            return true;
+        }
+        int isFound = false;
+        for(int adj:adjList[src]){
+            if(!visited[adj]){
+                isFound = isFound || isConnected(adj,target,visited,adjList);
+            }
+        }
+        return isFound;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int N = edges.size();
+        vector<int> adjList[N];
+        for(auto edge : edges){
+            vector<bool>visited(N,false);
+
+            if(isConnected(edge[0]-1, edge[1]-1, visited, adjList)){
+                return edge;
+            }
+            adjList[edge[0]-1].push_back(edge[1]-1);
+            adjList[edge[1]-1].push_back(edge[0]-1);
+        }
+        return {};
+    }
+};
