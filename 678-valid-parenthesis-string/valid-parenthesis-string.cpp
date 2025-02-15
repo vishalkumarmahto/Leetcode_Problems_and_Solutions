3975b8738a231ec9b6584d4a100b415e2d65cb70
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-02-15 18:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (76.64%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-        int dp[2][102] = {0}; // reduced space
-        dp[n & 1][0] = 1;     // base case
-        for (int i = n - 1; i >= 0; i--) {
-            char c = s[i];
-            for (int balance = 0; balance <= n; balance++) {
-                int ans = 0;
-                switch (c) {
-                case '(':
-                    ans = dp[(i + 1) & 1][balance + 1];
-                    break;
-                case ')':
-                    if (balance > 0)
-                        ans = dp[(i + 1) & 1][balance - 1];
-                    break;
-                case '*':
-                    ans |= dp[(i + 1) & 1][balance + 1] ||
-                           dp[(i + 1) & 1][balance];
-                    if (balance > 0)
-                        ans |= dp[(i + 1) & 1][balance - 1];
-                }
-                dp[i & 1][balance] = ans;
+        int mini = 0;
+        int maxi = 0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(') {
+                mini+=1;
+                maxi+=1;
             }
+            else if(s[i]==')'){
+                mini-=1;
+                maxi-=1;
+            }
+            else{
+                mini=mini-1;
+                maxi = maxi+1;
+            }
+            if(mini<0) mini=0;
+            if(maxi<0) return false;
         }
-        return dp[0][0] == 1;
+        return mini==0;
     }
 };
