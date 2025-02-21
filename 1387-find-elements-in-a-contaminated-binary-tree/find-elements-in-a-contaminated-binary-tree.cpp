0279e1a78e769c0923df45d8d467c0f5a88f4032
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-02-21 11:41:12 +0530

    Time: 11 ms (58.69%) | Memory: 34.6 MB (60.19%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_set<int> recoveredValues;
+    void recoverTree(TreeNode* root) {
+        if (!root)
+            return;
+        recoveredValues.insert(root->val);
+        if (root->left) {
+            root->left->val = 2 * root->val + 1;
+            recoverTree(root->left);
+        }
+        if (root->right) {
+            root->right->val = 2 * root->val + 2;
+            recoverTree(root->right);
+        }
+    }
+
+    FindElements(TreeNode* root) {
+        root->val = 0;
+        recoverTree(root);
+    }
+    bool find(int target) { return recoveredValues.count(target); 
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
