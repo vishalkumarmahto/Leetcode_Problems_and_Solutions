6bfa41c94aa24ded28ff57711ee66b5758377c5e
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-04-19 18:57:29 +0530

    Time: 17 ms (66.28%) | Memory: 38.9 MB (23.26%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,38 +3,33 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        // visited the distance matrix
+
         vector<vector<int>> vis(n, vector<int>(m, 0));
         vector<vector<int>> dist(n, vector<int>(m, 0));
-        // <coordinates, steps>
         queue<pair<pair<int, int>, int>> q;
-        // traverse the matrix
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                // start BFS if cell contains 0
                 if (mat[i][j] == 0) {
                     q.push({{i, j}, 0});
                     vis[i][j] = 1;
                 } else {
-                    // mark unvisited
+
                     vis[i][j] = 0;
                 }
             }
         }
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, +1, 0, -1};
-        // traverse till queue becomes empty
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
         while (!q.empty()) {
             int row = q.front().first.first;
             int col = q.front().first.second;
             int steps = q.front().second;
             q.pop();
             dist[row][col] = steps;
-            // for all 4 neighbours
             for (int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                // check for valid unvisited cell
+                int nrow = row + delRow[i];
+                int ncol = col + delCol[i];
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
                     vis[nrow][ncol] == 0) {
                     vis[nrow][ncol] = 1;
