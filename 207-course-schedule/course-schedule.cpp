e56857b38fd2f80df3ad65e3900cd91df1dc2b01
Vishal Kumar Mahto <132447693+vishalkumarmahto@users.noreply.github.com>
2025-05-30 19:30:15 +0530

    Time: 10 ms (28.15%) | Memory: 20.3 MB (26.08%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int V = numCourses;
-        vector<vector<int>> adj(V);
+        vector<vector<int>> adjList(V);
         for (auto it : prerequisites) {
             int course = it[0];
             int prereq = it[1];
-            adj[course].push_back(prereq);
+            adjList[course].push_back(prereq);
         }
-
-        vector<int> indegree(V, 0);
-        for (int i = 0; i < V; i++) {
-            for (auto it : adj[i]) {
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it:adjList[i]){
                 indegree[it]++;
             }
         }
-        queue<int> q;
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
         }
-        vector<int> topo;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            topo.push_back(node);
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0)
-                    q.push(it);
-            }
+        vector<int>topo;
+        while(!q.empty()){
+           int node = q.front();
+           q.pop();
+           topo.push_back(node);
+           for(auto it:adjList[node]){
+            indegree[it]--;
+            if(indegree[it]==0) q.push(it);
+           }
+
         }
-        if (topo.size() == V)
-            return true;
+        if(topo.size()==V) return true;
         return false;
     }
 };
